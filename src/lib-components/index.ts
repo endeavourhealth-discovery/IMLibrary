959502f15455b24e51da4affde323ffb0808ed02
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2022-02-15 14:37:29 +0000

    config debugging

--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
+export { default as ImLibrarySample2 } from "./im-library-sample-2.vue";
 export { default as ImLibrarySample } from "./im-library-sample.vue";
 export { default as TopBar } from "./TopBar.vue";
