688f0d8a5c48afccd9ac8cf7f9a99aa89eb1b5b0
matt-voror <matt@voror.co.uk>
2022-03-07 14:54:08 +0000

    added package as plugin changes. Added build folder for deployment

---
+++ docs/components/demo/SnomedLicense/Basic.vue
@@ -0,0 +1,3 @@
+<template>
+  <SnomedLicense />
+</template>
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-import { App } from "vue";
+import { App, Plugin } from "vue";
 import * as components from "./components";
-
-function install(app: App) {
-  for (const key in components) {
-    // @ts-expect-error
-    app.component(key, components[key]);
-  }
+export interface imlibraryInterface {
+  install: Plugin;
 }
+const IMLibrary: imlibraryInterface = {
+  install(app: App, options: any): void {
+    console.log(options);
+    if (!options || !options.store) {
+      throw new Error("Please initialise plugin with a Vuex store.");
+    }
+    if (options.store.state.docs) app.use(options.store);
+    for (const key in components) {
+      // @ts-expect-error
+      app.component(key, components[key]);
+    }
+  },
+};
 
 import "./assets/main.scss";
 
-export default { install };
+export default IMLibrary;
 
 export * from "./components";
 export * from "./constants";
