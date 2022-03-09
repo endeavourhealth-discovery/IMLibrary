2e582aa70e04a903ab6a94e996db10ed906ca13b
Kostas Kardatos <kon.kardatos@gmail.com>
2022-03-09 11:14:21 +0000

    update topbar
    remove unused lib folder
    
    update top bar

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { App, Plugin } from "vue";
 import * as components from "./components";
+import Tooltip from "primevue/tooltip";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
 export interface imlibraryInterface {
   install: Plugin;
 }
 const IMLibrary: imlibraryInterface = {
   install(app: App, options: any): void {
+    dom.watch();
+    library.add(fas as any, far as any);
     if (!options || !options.store) {
       throw new Error("Please initialise plugin with a Vuex store.");
     }
@@ -13,7 +21,9 @@ const IMLibrary: imlibraryInterface = {
       // @ts-expect-error
       app.component(key, components[key]);
     }
-  },
+    app.directive("tooltip", Tooltip);
+    app.component("font-awesome-icon", FontAwesomeIcon);
+  }
 };
 
 import "./assets/main.scss";
