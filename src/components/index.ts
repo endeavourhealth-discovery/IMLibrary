a0ed51ea6a7784cd8b88afe5932e6807b53e5545
matt-voror <matt@voror.co.uk>
2022-03-14 11:11:50 +0000

    moved TextDefinition into Library with tests. Modified to use store for defaultPredicateNames

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import ReportTable from "./ReportTable.vue";
 import SnomedLicense from "./SnomedLicense.vue";
 import TextHTMLWithLabel from "./TextHTMLWithLabel.vue";
 import TextWithLabel from "./TextWithLabel.vue";
+import TextDefinition from "./TextDefinition.vue"
 
-export {   TopBar, ReportTable, SnomedLicense, TextHTMLWithLabel, TextWithLabel };
+export {   TopBar, ReportTable, SnomedLicense, TextHTMLWithLabel, TextWithLabel, TextDefinition };
