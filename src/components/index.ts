450fa99f350d76fe52353b5f3876d7476c68ce5a
Kostas Kardatos <kon.kardatos@gmail.com>
2022-03-15 16:00:01 +0200

    add term codes tab component + update vocab

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,8 +6,22 @@ import ReportTable from "./modules/ReportTable.vue";
 import SnomedLicense from "./modules/SnomedLicense.vue";
 import TextHTMLWithLabel from "./modules/TextHTMLWithLabel.vue";
 import TextWithLabel from "./modules/TextWithLabel.vue";
-import TextDefinition from "./modules/TextDefinition.vue"
+import TextDefinition from "./modules/TextDefinition.vue";
 import ObjectNameWithLabel from "./modules/ObjectNameWithLabel.vue";
 import NumberWithLabel from "./modules/NumberWithLabel.vue";
+import TermCodeTable from "./modules/TermCodeTable.vue";
 
-export { ArrayObjectNameTagWithLabel, ArrayObjectNamesToStringWithLabel, ArrayObjectNameListboxWithLabel, TopBar, ReportTable, SnomedLicense, TextHTMLWithLabel, TextWithLabel, TextDefinition, ObjectNameWithLabel, NumberWithLabel };
+export {
+  ArrayObjectNameTagWithLabel,
+  ArrayObjectNamesToStringWithLabel,
+  ArrayObjectNameListboxWithLabel,
+  TopBar,
+  ReportTable,
+  SnomedLicense,
+  TextHTMLWithLabel,
+  TextWithLabel,
+  TextDefinition,
+  ObjectNameWithLabel,
+  NumberWithLabel,
+  TermCodeTable
+};
