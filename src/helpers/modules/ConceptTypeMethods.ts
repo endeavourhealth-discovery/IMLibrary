f12ce33069a0f30682cc6885fa0fdeeadd7c05a3
matt-voror <matt@voror.co.uk>
2022-03-31 12:55:21 +0100

    added getNamesAsStringFromTypes to helpers

--- src/helpers/modules/ConceptTypeMethods.ts
+++ src/helpers/modules/ConceptTypeMethods.ts
@@ -5,6 +5,7 @@
 
 import { TTIriRef } from "../../interfaces/modules/TTIriRef";
 import { Vocabulary } from "../../vocabulary/index";
+import palette from "google-palette";
 
 export function isOfTypes(
   conceptTypeElements: TTIriRef[],
@@ -48,51 +49,44 @@ export function getFAIconFromType(conceptTypes: TTIriRef[]): string[] {
   if (isOfTypes(conceptTypes, Vocabulary.SHACL.NODESHAPE)) {
     return ["fas", "project-diagram"];
   }
-
   if (isProperty(conceptTypes)) {
     return ["far", "edit"];
   }
-
   if (isValueSet(conceptTypes)) {
     return ["fas", "tasks"];
   }
-
   if (isOfTypes(conceptTypes, Vocabulary.IM.FOLDER)) {
     return ["fas", "folder"];
   }
-
   if (isOfTypes(conceptTypes, Vocabulary.IM.QUERY_TEMPLATE)) {
     return ["fas", "search"];
   }
   return ["far", "lightbulb"];
 }
 
-import palette from "google-palette";
 export function getColourFromType(conceptTypes: TTIriRef[]): string {
   const bgs = palette("tol-rainbow", 6);
   const bgsFixed = bgs.map((color: string) => "#" + color + "88");
-
   if (isOfTypes(conceptTypes, Vocabulary.SHACL.NODESHAPE)) {
     return bgsFixed[0];
   }
-
   if (isProperty(conceptTypes)) {
     return bgsFixed[5];
   }
-
   if (isValueSet(conceptTypes)) {
     return bgsFixed[2];
   }
-
   if (isOfTypes(conceptTypes, Vocabulary.IM.FOLDER)) {
     return bgsFixed[1];
   }
-
   if (isOfTypes(conceptTypes, Vocabulary.IM.QUERY_TEMPLATE)) {
     return bgsFixed[3];
   }
-
   return bgsFixed[4];
+}
+
+export function getNamesAsStringFromTypes(typeList: TTIriRef[]) {
+  return typeList.map((type) => type.name).join(", ");
 }
 
 export default {
@@ -101,4 +95,5 @@ export default {
   isValueSet,
   getColourFromType,
   getFAIconFromType,
+  getNamesAsStringFromTypes,
 };
