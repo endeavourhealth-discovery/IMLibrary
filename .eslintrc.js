91359b96a293291085765e99136c349cd0958dcf
matt-voror <matt@voror.co.uk>
2022-04-01 15:09:12 +0100

    linter settings updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,23 @@ module.exports = {
   root: true,
 
   env: {
-    node: true,
+    node: true
   },
 
-  extends: ["plugin:vue/vue3-essential"],
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
+    "@vue/typescript"
+  ],
 
   plugins: ["prettier"],
 
   parserOptions: {
     ecmaVersion: 2020,
-    parser: "@typescript-eslint/parser",
+    parser: "@typescript-eslint/parser"
   },
 
   ignorePatterns: ["src/discovery-syntax/*", "tests/*"],
@@ -19,10 +26,9 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-unused-vars": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-use-before-define":
-      process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-use-before-define": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "@typescript-eslint/no-var-requires": 0,
-    "@typescript-eslint/no-explicit-any": "off",
-  },
+    "@typescript-eslint/no-explicit-any": "off"
+  }
 };
