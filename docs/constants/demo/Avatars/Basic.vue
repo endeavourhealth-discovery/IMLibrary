cfe65efbca73b5ec7fa885240c6a7baf2969ecab
matt-voror <matt@voror.co.uk>
2022-04-07 15:59:57 +0100

    improved layout of avatars doc

--- docs/constants/demo/Avatars/Basic.vue
+++ docs/constants/demo/Avatars/Basic.vue
@@ -1,10 +1,14 @@
 <template>
-  <ul>
-    <li v-for="avatar of avatars">{{ avatar }}</li>
-  </ul>
+  <div>
+    <span>[</span>
+    <ul>
+      <li v-for="avatar of avatars">"{{ avatar }}",</li>
+    </ul>
+    <span>]</span>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 import { Avatars } from "../../../../src/constants/modules/Avatars";
 export default {
   data() {
@@ -15,4 +19,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  margin: 0 0 0 1.25rem;
+  padding: 0;
+  list-style-type: none;
+}
+</style>
