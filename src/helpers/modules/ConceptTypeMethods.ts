5b6b63b2bdafba46e238c66eb5ebef1076e41d00
Richard Collier <gitkraken@tirinoarim.co.uk>
2022-04-13 12:47:07 +0100

    Fix Query type checks

--- src/helpers/modules/ConceptTypeMethods.ts
+++ src/helpers/modules/ConceptTypeMethods.ts
@@ -50,7 +50,7 @@ export function isConcept(conceptTypes: TTIriRef[]): boolean {
 }
 
 export function isQuery(entityTypes: TTIriRef[]): boolean {
-  return isOfTypes(entityTypes, Vocabulary.IM.QUERY_TEMPLATE);
+  return isOfTypes(entityTypes, Vocabulary.IM.QUERY);
 }
 
 export function isRecordModel(entityTypes: TTIriRef[]): boolean {
@@ -71,10 +71,10 @@ export function getFAIconFromType(conceptTypes: TTIriRef[]): string[] {
   if (isValueSet(conceptTypes)) {
     return ["fas", "tasks"];
   }
-  if (isOfTypes(conceptTypes, Vocabulary.IM.FOLDER)) {
+  if (isFolder(conceptTypes)) {
     return ["fas", "folder"];
   }
-  if (isOfTypes(conceptTypes, Vocabulary.IM.QUERY)) {
+  if (isQuery(conceptTypes)) {
     return ["fas", "search"];
   }
   return ["far", "lightbulb"];
@@ -92,10 +92,10 @@ export function getColourFromType(conceptTypes: TTIriRef[]): string {
   if (isValueSet(conceptTypes)) {
     return bgsFixed[2];
   }
-  if (isOfTypes(conceptTypes, Vocabulary.IM.FOLDER)) {
+  if (isFolder(conceptTypes)) {
     return bgsFixed[1];
   }
-  if (isOfTypes(conceptTypes, Vocabulary.IM.QUERY)) {
+  if (isQuery(conceptTypes)) {
     return bgsFixed[3];
   }
   return bgsFixed[4];
