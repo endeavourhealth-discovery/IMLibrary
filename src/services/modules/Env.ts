b05ac69b8a14729427eed299b81746465bc4f019
matt-voror <matt@voror.co.uk>
2022-05-18 08:55:31 +0100

    ENV values capitalised as final

--- src/services/modules/Env.ts
+++ src/services/modules/Env.ts
@@ -1,22 +1,10 @@
 const Env = {
-  api: import.meta.env.VITE_API
-    ? (import.meta.env.VITE_API as string)
-    : "/imapi/",
-  authUrl: import.meta.env.VITE_AUTH_URL
-    ? (import.meta.env.VITE_AUTH_URL as string)
-    : "/auth/",
-  directoryUrl: import.meta.env.VITE_DIRECTORY_URL
-    ? (import.meta.env.VITE_DIRECTORY_URL as string)
-    : "/",
-  editorUrl: import.meta.env.VITE_EDITOR_URL
-    ? (import.meta.env.VITE_EDITOR_URL as string)
-    : "/editor/",
-  catalogueUrl: import.meta.env.VITE_CATALOGUE_URL
-    ? (import.meta.env.VITE_CATALOGUE_URL as string)
-    : "/catalogue/",
-  viewerUrl: import.meta.env.VITE_VIEWER_URL
-    ? (import.meta.env.VITE_VIEWER_URL as string)
-    : "/viewer/"
+  API: import.meta.env.VITE_API ? (import.meta.env.VITE_API as string) : "/imapi/",
+  AUTH_URL: import.meta.env.VITE_AUTH_URL ? (import.meta.env.VITE_AUTH_URL as string) : "/auth/",
+  DIRECTORY_URL: import.meta.env.VITE_DIRECTORY_URL ? (import.meta.env.VITE_DIRECTORY_URL as string) : "/",
+  EDITOR_URL: import.meta.env.VITE_EDITOR_URL ? (import.meta.env.VITE_EDITOR_URL as string) : "/editor/",
+  CATALOGUE_URL: import.meta.env.VITE_CATALOGUE_URL ? (import.meta.env.VITE_CATALOGUE_URL as string) : "/catalogue/",
+  VIEWER_URL: import.meta.env.VITE_VIEWER_URL ? (import.meta.env.VITE_VIEWER_URL as string) : "/viewer/"
 };
 
 Object.freeze(Env);
