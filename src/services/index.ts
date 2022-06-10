a1b74c31b5e8f667405b44e15951c6ec028a0acb
matt-voror <matt@voror.co.uk>
2022-06-10 13:28:59 +0100

    moved setService into library

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,5 +3,6 @@ import DirectService from "./modules/DirectService";
 import EntityService from "./modules/EntityService";
 import Env from "./modules/Env";
 import LoggerService from "./modules/LoggerService";
+import SetService from "./modules/SetService";
 
-export { ConfigService, DirectService, EntityService, Env, LoggerService };
+export { ConfigService, DirectService, EntityService, Env, LoggerService, SetService };
