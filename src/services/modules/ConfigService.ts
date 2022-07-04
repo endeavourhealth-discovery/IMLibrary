ad46e85d5ac42f3c60c11efc48787f8de287452b
Kostas Kardatos <kon.kardatos@gmail.com>
2022-07-04 12:40:05 +0300

    remove commented out config calls

--- src/services/modules/ConfigService.ts
+++ src/services/modules/ConfigService.ts
@@ -31,36 +31,4 @@ export default class ConfigService {
       return [] as DashboardLayout[];
     }
   }
-
-  // public async getFilterDefaults(): Promise<FilterDefaultsConfig> {
-  //   try {
-  //     return await this.axios.get(Env.API + "api/config/public/filterDefaults");
-  //   } catch (error) {
-  //     return {} as FilterDefaultsConfig;
-  //   }
-  // }
-
-  // public async getDefaultPredicateNames(): Promise<any> {
-  //   try {
-  //     return await this.axios.get(Env.API + "api/config/public/defaultPredicateNames");
-  //   } catch (error) {
-  //     return {} as any;
-  //   }
-  // }
-
-  // public async getGraphExcludePredicates(): Promise<any> {
-  //   try {
-  //     return await this.axios.get(Env.API + "api/config/public/graphExcludePredicates");
-  //   } catch (error) {
-  //     return [] as string[];
-  //   }
-  // }
-
-  // public async getXmlSchemaDataTypes(): Promise<any> {
-  //   try {
-  //     return await this.axios.get(Env.API + "api/config/public/xmlSchemaDataTypes");
-  //   } catch (error) {
-  //     return [] as string[];
-  //   }
-  // }
 }
