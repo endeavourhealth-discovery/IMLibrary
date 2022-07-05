0ee2626ca8cf4ec58a882c179e64b7675ba2b275
Kostas Kardatos <kon.kardatos@gmail.com>
2022-07-05 12:52:24 +0300

    update data model type

--- src/config/modules/FilterDefaults.ts
+++ src/config/modules/FilterDefaults.ts
@@ -1,10 +1,10 @@
 import { SortDirection } from "../../models/modules/Search";
-import { IM, RDF, RDFS } from "../../vocabulary/Vocabulary";
+import { IM, RDF, RDFS, SHACL } from "../../vocabulary/Vocabulary";
 
 const FILTER_DEFAULTS = {
   schemeOptions: [IM.NAMESPACE, IM.DOMAIN + "sct#"],
   statusOptions: [IM.ACTIVE, IM.DRAFT],
-  typeOptions: [IM.CONCEPT, IM.VALUE_SET, IM.CONCEPT_SET, IM.DATAMODEL_ENTITY, IM.DATAMODEL_PROPERTY, IM.QUERY, RDFS.CLASS, RDF.PROPERTY],
+  typeOptions: [IM.CONCEPT, IM.VALUE_SET, IM.CONCEPT_SET, SHACL.NODESHAPE, IM.DATAMODEL_PROPERTY, IM.QUERY, RDFS.CLASS, RDF.PROPERTY],
   sortFields: [{ label: "Usage", value: "weighting" }],
   sortDirections: [
     { label: "Descending", value: SortDirection.DESC },
