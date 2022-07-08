ea38ca88cdf3a81bd454fd28bb59ccff470bde43
Kostas Kardatos <kon.kardatos@gmail.com>
2022-07-08 11:28:19 +0300

    include im:Folder in type filters

--- src/config/modules/FilterDefaults.ts
+++ src/config/modules/FilterDefaults.ts
@@ -4,7 +4,7 @@ import { IM, RDF, RDFS, SHACL } from "../../vocabulary/Vocabulary";
 const FILTER_DEFAULTS = {
   schemeOptions: [IM.NAMESPACE, "http://snomed.info/sct#"],
   statusOptions: [IM.ACTIVE, IM.DRAFT],
-  typeOptions: [IM.CONCEPT, IM.VALUE_SET, IM.CONCEPT_SET, SHACL.NODESHAPE, IM.DATAMODEL_PROPERTY, IM.QUERY, RDFS.CLASS, RDF.PROPERTY],
+  typeOptions: [IM.CONCEPT, IM.VALUE_SET, IM.CONCEPT_SET, SHACL.NODESHAPE, IM.DATAMODEL_PROPERTY, IM.QUERY, RDFS.CLASS, RDF.PROPERTY, IM.FOLDER],
   sortFields: [{ label: "Usage", value: "weighting" }],
   sortDirections: [
     { label: "Descending", value: SortDirection.DESC },
