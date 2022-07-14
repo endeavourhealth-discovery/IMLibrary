ba18c067374745fc605e6e5a11a8016d8e8bbbc8
matt-voror <matt@voror.co.uk>
2022-07-14 12:11:42 +0100

    added entityTypes as own config outside filterOptions

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,7 +1,8 @@
 import DefaultPredicateNames from "./modules/DefaultPredicateNames";
+import EntityTypes from "./modules/EntityTypes";
 import FilterDefaults from "./modules/FilterDefaults";
 import GraphExcludePredicates from "./modules/GraphExcludePredicates";
 import TextDefinitionExcludePredicates from "./modules/TextDefinitionExcludePredicates";
 import XmlSchemaDatatypes from "./modules/XmlSchemaDatatypes";
 
-export { DefaultPredicateNames, FilterDefaults, GraphExcludePredicates, TextDefinitionExcludePredicates, XmlSchemaDatatypes };
+export { DefaultPredicateNames, EntityTypes, FilterDefaults, GraphExcludePredicates, TextDefinitionExcludePredicates, XmlSchemaDatatypes };
