3e48c9a1c4da1d4c36e0212950e1c172ba7b9fae
matt-voror <matt@voror.co.uk>
2022-07-14 12:14:37 +0100

    added editor mode as enum

--- src/enums/Enums.ts
+++ src/enums/Enums.ts
@@ -1,9 +1,10 @@
 import { BuilderType } from "./modules/BuilderType";
 import { ComponentType } from "./modules/ComponentType";
 import { ECLComponent } from "./modules/ECLComponent";
+import { EditorMode } from "./modules/EditorMode";
 import { PasswordStrength } from "./modules/PasswordStrength";
 import { QueryComponentType } from "./modules/QueryComponentType";
 import { SortBy } from "./modules/SortBy";
 import { SortDirection } from "./modules/SortDirection";
 
-export { BuilderType, ComponentType, ECLComponent, PasswordStrength, QueryComponentType, SortBy, SortDirection };
+export { BuilderType, ComponentType, ECLComponent, EditorMode, PasswordStrength, QueryComponentType, SortBy, SortDirection };
