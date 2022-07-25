2d04d4393552753b903592b454234ecb1bace335
matt-voror <matt@voror.co.uk>
2022-07-25 17:48:20 +0100

    added shape dynamic types

--- src/enums/modules/ComponentType.ts
+++ src/enums/modules/ComponentType.ts
@@ -7,5 +7,12 @@ export enum ComponentType {
   HAS_MEMBER = "HasMember",
   REFINEMENT = "Refinement",
   QUANTIFIER = "Quantifier",
-  PROPERTY = "Property"
+  PROPERTY = "Property",
+  GROUP = "Group",
+  ENTITY_MULTI_DROPDOWN = "EntityMultiDropdown",
+  ENTITY_MULTI_SEARCH = "EntityMultiSearch",
+  ENTITY_SINGLE_DROPDOWN = "EntitySingleDropdown",
+  HTML_SINGLE_SELECT = "HtmlSingleSelect",
+  STRING_SINGLE_DISPLAY = "StringSingleDisplay",
+  STRING_SINGLE_SELECT = "StringSingleSelect"
 }
