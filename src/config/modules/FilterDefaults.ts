ba516db9bcda020f8becfd84faa7e77522666537
Zeynep Bayazitoglu <zeynep.bayazitoglu@ergosoft.co.uk>
2022-07-25 09:08:19 +0100

    Fix search result default filters

--- src/config/modules/FilterDefaults.ts
+++ src/config/modules/FilterDefaults.ts
@@ -5,11 +5,8 @@ const FILTER_DEFAULTS = {
   schemeOptions: [IM.NAMESPACE, "http://snomed.info/sct#"],
   statusOptions: [IM.ACTIVE, IM.DRAFT],
   typeOptions: [IM.CONCEPT, IM.VALUE_SET, IM.CONCEPT_SET, SHACL.NODESHAPE, IM.DATAMODEL_PROPERTY, IM.QUERY, RDFS.CLASS, RDF.PROPERTY, IM.FOLDER],
-  sortFields: [{ label: "Usage", value: "weighting" }],
-  sortDirections: [
-    { label: "Descending", value: SortDirection.DESC },
-    { label: "Ascending", value: SortDirection.ASC }
-  ]
+  sortField: "weighting",
+  sortDirection: SortDirection.DESC
 };
 
 export default { ...FILTER_DEFAULTS };
