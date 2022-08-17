564f9b714746bbe432b4adb6df62e90f4517e444
Richard Collier <gitkraken@tirinoarim.co.uk>
2022-08-17 13:40:47 +0100

    Updated filer service to allow correct handling

--- src/services/modules/FilerService.ts
+++ src/services/modules/FilerService.ts
@@ -8,18 +8,14 @@ export default class FilerService {
     this.axios = axios;
   }
 
-  public async moveFolder(entity: string, oldFolder: string, newFolder: string): Promise<boolean> {
-    try {
-      return await this.axios.post(this.api + "api/filer/folder/move", null, {
-        params: {
-          entity: entity,
-          oldFolder: oldFolder,
-          newFolder: newFolder
-        }
-      });
-    } catch (error) {
-      return false;
-    }
+  public moveFolder(entity: string, oldFolder: string, newFolder: string): Promise<void> {
+    return this.axios.post(this.api + "api/filer/folder/move", null, {
+      params: {
+        entity: entity,
+        oldFolder: oldFolder,
+        newFolder: newFolder
+      }
+    });
   }
 
   public async createFolder(container: string, name: string): Promise<string> {
@@ -33,13 +29,5 @@ export default class FilerService {
 
   public async downloadDeltas(): Promise<any> {
     return this.axios.get(this.api + "api/filer/deltas/download", { responseType: "blob" });
-    // const client = this.axios.create({
-    //   baseURL: this.api,
-    //   timeout: 0
-    // });
-    //
-    // return client.get("api/filer/deltas/download", {
-    //   responseType: "blob"
-    // });
   }
 }
