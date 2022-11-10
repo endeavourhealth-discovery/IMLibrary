ad0cb063db0b431dc68e88bf892d47b67d91c82f
kkardatos <kon.kardatos@gmail.com>
2022-11-10 13:33:05 +0200

    add getQueryFromECL getECLFromQuery isValidECL calls

--- src/services/modules/SetService.ts
+++ src/services/modules/SetService.ts
@@ -1,5 +1,5 @@
 import { CancelToken } from "axios";
-import { SearchResponse } from "../../interfaces/Interfaces";
+import { Query, SearchResponse } from "../../interfaces/Interfaces";
 import Env from "./Env";
 
 export default class SetService {
@@ -19,6 +19,22 @@ export default class SetService {
     } catch (error) {
       return {} as SearchResponse;
     }
+  }
+
+  public async getQueryFromECL(ecl: string): Promise<Query> {
+    return this.axios.get(Env.API + "api/set/public/ecl/query", {
+      params: { ecl: ecl }
+    });
+  }
+
+  public async isValidECL(ecl: string): Promise<boolean> {
+    return this.axios.get(Env.API + "api/set/public/ecl/validity", {
+      params: { ecl: ecl }
+    });
+  }
+
+  public async getECLFromQuery(query: Query): Promise<string> {
+    return this.axios.post(Env.API + "api/set/public/query/ecl", query);
   }
 
   public async publish(conceptIri: string) {
